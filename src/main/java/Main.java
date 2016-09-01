ac700372d1bfaec788a1063f394ce7299450dab0
Cas Eliëns <cas.eliens@gmail.com>
2016-09-01 14:08:46 +0200

    they're doing stuff with my power supply D:

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,10 @@ import io.github.swagger2markup.Swagger2MarkupConfig;
 import io.github.swagger2markup.Swagger2MarkupConverter;
 import io.github.swagger2markup.builder.Swagger2MarkupConfigBuilder;
 import io.github.swagger2markup.markup.builder.MarkupLanguage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
@@ -15,24 +16,17 @@ import java.time.LocalDateTime;
  * Created by cas.eliens on 31-Aug-16.
  */
 public class Main {
-        public static void main( String[] args ) throws MalformedURLException {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Main.class);
 
-            LocalDateTime dt = LocalDateTime.now();
+        logger.info("Hello World");
 
-            // Versioned names for debugging
-            String year = String.valueOf(dt.getYear());
-            String month = String.valueOf(dt.getMonthValue());
-            String day = String.valueOf(dt.getDayOfMonth());
-            String hour = String.valueOf(dt.getHour());
-            String minute = String.valueOf(dt.getMinute());
-            String second = String.valueOf(dt.getSecond());
-
-            String hash = String.format("%4s_%2s_%2s-%2s_%2s_%2s", year, month, day, hour, minute, second).replace(' ', '0');
-
-
+        try {
+            File folder = new File("build/asciidoc");
+            emptyFolder(folder);
             //URL swaggerFile = new URL("http://petstore.swagger.io/v2/swagger.json");
             Path swaggerFile = Paths.get("spec.yaml");
-            Path outputFile = Paths.get("build/asciidoc/api_" + hash);
+            Path outputFile = Paths.get("build/asciidoc/api_" + getDateString());
 
             Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
                     .withMarkupLanguage(MarkupLanguage.ASCIIDOC)
@@ -46,6 +40,36 @@ public class Main {
                     .withConfig(config)
                     .build();
 
+
             converter.toFile(outputFile);
+        } catch (Exception ex) {
+            ex.printStackTrace(System.err);
         }
+    }
+
+    /**
+     * Generate a string representing the current date and time
+     *
+     * @return String, formatted yyyy_MM_dd-HH_mm_ss
+     */
+    private static String getDateString() {
+        LocalDateTime dt = LocalDateTime.now();
+        String year = String.valueOf(dt.getYear());
+        String month = String.valueOf(dt.getMonthValue());
+        String day = String.valueOf(dt.getDayOfMonth());
+        String hour = String.valueOf(dt.getHour());
+        String minute = String.valueOf(dt.getMinute());
+        String second = String.valueOf(dt.getSecond());
+
+        return String.format("%4s_%2s_%2s-%2s_%2s_%2s", year, month, day, hour, minute, second).replace(' ', '0');
+    }
+
+    public static void emptyFolder(File folder) {
+        File[] files = folder.listFiles();
+        if (files != null) { //some JVMs return null for empty dirs
+            for (File f : files) {
+                f.delete();
+            }
+        }
+    }
 }
